43c660b225fa43958091631df6c530a438809b27
Aleksei Surkov <almihsur@gmail.com>
2022-02-21 23:21:25 +0300

    make the Task_1_1

---
+++ itProgger/src/itProger/Task_1_1.java
@@ -0,0 +1,30 @@
+package itProger;
+//Создайте программу, которая выводит строку и подсчитывает в ней количество:
+//букв;
+//пробелов;
+//чисел;
+//символов (здесь имеется ввиду символы по типу , и !).
+public class Task_1_1 {
+    public static void main(String[] args) {
+     String s = "Hello, I am the 1!";
+        int sym = 0;
+        int space = 0;
+        int dig = 0;
+        int other = 0;
+     char[] arrChar = s.toCharArray();
+
+     for (int i = 0; i < arrChar.length; i++){
+         if (Character.isLetter(arrChar[i]))
+             sym++;
+        else if (Character.isSpaceChar(arrChar[i]))
+             space++;
+        else if (Character.isDigit(arrChar[i]))
+             dig++;
+         else other++;
+     }
+        System.out.println("букв: " + sym);
+        System.out.println("пробелов "+ space);
+        System.out.println("чисел "+ dig);
+        System.out.println("символов "+ other);
+    }
+}
